ba08f87626d7d5f30410afb0cb0007f94c23c2db
stu-ChrisA <92584525+stu-ChrisA@users.noreply.github.com>
2021-11-05 10:22:25 -0400

    Update GuessingGame.java
    adding attempts counter and congratulations message.

--- GuessingGame.java
+++ GuessingGame.java
@@ -8,6 +8,7 @@ public class GuessingGame
         System.out.println("Welcome to my guessing game");
         System.out.println("Please guess a number between 1 - 100"); //Starts the game and ask user for input
         int personGuess = input.nextInt();
+        int attemps = 0;
         int randomNumber = (int)((Math.random() * 100 - 1) + 1); //Computer generates number
         while(personGuess != randomNumber) // Checks if the numbers are not the same
         {
@@ -15,14 +16,22 @@ public class GuessingGame
               statement and ask for input again*/
             if(personGuess > randomNumber) 
             {
+                attemps++;
                 System.out.println("Too high");
                 personGuess = input.nextInt();
             }
             else if(personGuess < randomNumber)
             {
+                attemps++;
                 System.out.println("Too low");
                 personGuess = input.nextInt();
             }
+        }
+        System.out.println("You got it!");
+        System.out.println("It took you " + attemps + " attemps.");
+        if(attemps <= 7)
+        {
+            System.out.println("Wow you're a quick guesser!");
         }
     }
 }
